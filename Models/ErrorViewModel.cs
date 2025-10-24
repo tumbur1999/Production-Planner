f08ca8a853d7311c976dbc7ed78319b24d025ae8
tumbur1999 <tumbur1999@gmail.com>
2025-10-24 15:57:07 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProductionPlanner.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
